88d7aab893c7e8cf779e51d5d736a41856766f8a
AceReX <ace.rexnoir@gmail.com>
2025-10-31 20:30:02 +0530

    feat : add migration for menu and menu items

---
+++ database/migrations/create_menu_items_table.php
@@ -0,0 +1,22 @@
+<?php
+
+return new class extends Illuminate\Database\Migrations\Migration {
+
+    public function up(): void
+    {
+        Illuminate\Support\Facades\Schema::create('menu_items', function (Illuminate\Database\Schema\Blueprint $table) {
+            $table->id();
+            $table->foreignId('menu_id')->constrained('menus')->onDelete('cascade');
+            $table->string('title');
+            $table->string('url');
+            $table->integer('order')->default(0);
+            $table->timestamps();
+            $table->nestedSet();
+        });
+    }
+
+    public function down(): void
+    {
+        Illuminate\Support\Facades\Schema::dropIfExists('menu_items');
+    }
+};
