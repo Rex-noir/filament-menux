50b42ce0be8d86914244e6fc37297dc520bd07b1
AceReX <ace.rexnoir@gmail.com>
2025-11-01 10:30:50 +0530

    feat : add support for static menus

--- src/Filament/Resources/Menus/Pages/ListMenus.php
+++ src/Filament/Resources/Menus/Pages/ListMenus.php
@@ -3,7 +3,12 @@
 namespace AceREx\FilamentMenux\Filament\Resources\Menus\Pages;
 
 use AceREx\FilamentMenux\Filament\Resources\Menus\MenuResource;
+use AceREx\FilamentMenux\FilamentMenuxPlugin;
+use AceREx\FilamentMenux\Models\Menu;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Forms\Components\CheckboxList;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
 
 class ListMenus extends ListRecords
@@ -12,8 +17,45 @@ class ListMenus extends ListRecords
 
     protected function getHeaderActions(): array
     {
+        $plugin = FilamentMenuxPlugin::get();
+        $staticMenus = $plugin->getStaticMenus();
+        $existingSlugs = Menu::whereIn('slug', $staticMenus->keys())->pluck('slug');
+
+        if ($staticMenus->count() ===  $existingSlugs->count()) {
+            return [];
+        }
+
+        if ($staticMenus !== null && $staticMenus->isNotEmpty()) {
+
+            return [
+                Action::make('create')
+                    ->label("Add Menu")
+                    ->schema([
+                        CheckboxList::make('selected_menu_items')
+                            ->label("Select menus to create")
+                            ->options($staticMenus->toArray())
+                            ->disableOptionWhen(fn($value, $key): bool => in_array($value, $existingSlugs->toArray()))
+                            ->helperText("Menus that already exist are disabled.")
+                            ->required(),
+                    ])
+                    ->action(function (array $data) use ($staticMenus) {
+                        foreach ($data['selected_menu_items'] as $slug) {
+                            Menu::firstOrCreate([
+                                'slug' => $slug,
+                            ], [
+                                'name' => $staticMenus->get($slug),
+                            ]);
+                        }
+                        Notification::make()
+                            ->title('Menus created successfully.')
+                            ->success()
+                            ->send();
+                    }),
+            ];
+        }
+
         return [
-            CreateAction::make(),
+            CreateAction::make()
         ];
     }
 }
