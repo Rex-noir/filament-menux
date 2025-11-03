e51f7239040572024e7d4680a7b7719e5766882f
AceReX <ace.rexnoir@gmail.com>
2025-11-03 21:47:27 +0530

    enhance: improve grid layout responsiveness in `edit-menu` view
    - Adjusted grid layout by adding `space-y-4` for unsupported screen sizes and ensuring better adaptability in `md` viewports.

--- resources/views/pages/edit-menu.blade.php
+++ resources/views/pages/edit-menu.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     {{$this->form}}
-    <div class="grid grid-cols-5 gap-5">
+    <div class="space-y-4 md:grid grid-cols-5 gap-5">
         <div class="col-span-2">
             @livewire('menu-item-tabs', ['menuId' => $this->record->id])
         </div>
