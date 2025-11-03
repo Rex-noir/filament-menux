13a22f9526b9c31ae5b97cd181b0ecef6d2e99ca
AceReX <ace.rexnoir@gmail.com>
2025-11-03 11:25:29 +0530

    feat: restructure edit-menu layout to include menu-item form alongside menu-items-builder

--- resources/views/pages/edit-menu.blade.php
+++ resources/views/pages/edit-menu.blade.php
@@ -1,6 +1,11 @@
 <x-filament-panels::page>
     {{$this->form}}
-    <div>
-        @livewire('menu-items-builder', ['menuId'=>$this->record->id]))
+    <div class="grid grid-cols-5">
+        <div class="col-span-2">
+            @livewire('menu-item-form', ['menuId' => $this->record->id])
+        </div>
+        <div>
+            @livewire('menu-items-builder', ['menuId'=>$this->record->id])
+        </div>
     </div>
 </x-filament-panels::page>
