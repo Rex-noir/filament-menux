ea1c1d66775a77382627786d3cc5ab9a3dc0121c
AceReX <ace.rexnoir@gmail.com>
2025-11-04 17:15:37 +0530

    refactor: replace direct `MenusTable` reference with dynamic retrieval via `FilamentMenuxPlugin`
    - Updated `MenuResource` to use dynamically resolved `MenusTable` from `FilamentMenuxPlugin` through `getMenusTable`.
    - Improved configurability by eliminating hardcoded `MenusTable` reference.
    - Removed unused imports for a cleaner codebase.

--- src/Filament/Resources/Menus/MenuResource.php
+++ src/Filament/Resources/Menus/MenuResource.php
@@ -5,13 +5,11 @@ namespace AceREx\FilamentMenux\Filament\Resources\Menus;
 use AceREx\FilamentMenux\Filament\Resources\Menus\Pages\CreateMenu;
 use AceREx\FilamentMenux\Filament\Resources\Menus\Pages\EditMenu;
 use AceREx\FilamentMenux\Filament\Resources\Menus\Pages\ListMenus;
-use AceREx\FilamentMenux\Filament\Resources\Menus\Schemas\MenuForm;
 use AceREx\FilamentMenux\Filament\Resources\Menus\Tables\MenusTable;
 use AceREx\FilamentMenux\FilamentMenuxPlugin;
 use AceREx\FilamentMenux\Models\Menu;
 use BackedEnum;
 use Filament\Resources\Resource;
-use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Contracts\Support\Htmlable;
@@ -40,7 +38,10 @@ class MenuResource extends Resource
 
     public static function table(Table $table): Table
     {
-        return MenusTable::configure($table);
+        /** @var MenusTable $menusTable */
+        $menusTable = FilamentMenuxPlugin::get()->getMenusTable();
+
+        return $menusTable::configure($table);
     }
 
     public static function getRelations(): array
