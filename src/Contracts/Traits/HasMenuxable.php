7c6568fdc865a2e28ce1443149d7cc4335cc5b3b
AceReX <ace.rexnoir@gmail.com>
2025-11-12 16:56:08 +0530

    feat: implement `HasMenuxable` trait with `getMenuxableGroups`, `getMenuxablesUsing`, and `getMenuxTarget` methods

--- src/Contracts/Traits/HasMenuxable.php
+++ src/Contracts/Traits/HasMenuxable.php
@@ -2,4 +2,25 @@
 
 namespace AceREx\FilamentMenux\Contracts\Traits;
 
-trait HasMenuxable {}
+use AceREx\FilamentMenux\Contracts\Enums\MenuxLinkTarget;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+
+trait HasMenuxable
+{
+    public static function getMenuxableGroups(): Collection
+    {
+        return collect([]);
+    }
+
+    public static function getMenuxablesUsing(Builder $builder, int $page, int $perPage, ?string $q): Builder | LengthAwarePaginator
+    {
+        return $builder;
+    }
+
+    public function getMenuxTarget(): \BackedEnum
+    {
+        return MenuxLinkTarget::SELF;
+    }
+}
